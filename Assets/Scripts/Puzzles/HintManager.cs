c8ff4d17f20bb5848cfb85f6e0f5f7b6e973f1b0
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-02-18 21:11:30 +0600

    Initial hint manager commit

---
+++ Assets/Scripts/Puzzles/HintManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintManager : MonoBehaviour
+{
+    public string message = "You need to place boxes of top of pressure plates";
+    private bool open = false;
+
+    public void interact()
+    {
+        open = !open;
+        Time.timeScale = (open) ? 0f : 1f;
+
+        if (open)
+            Debug.Log(message);
+    }
+}
