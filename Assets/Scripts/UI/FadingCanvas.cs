e7317827253e18e07287b4fcfb1459960f7b0544
joeltrenchard <jct12@student.london.ac.uk>
2024-03-01 14:13:30 +0000

    Fixed bugs

--- Assets/Scripts/UI/FadingCanvas.cs
+++ Assets/Scripts/UI/FadingCanvas.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FadeCanvas : MonoBehaviour
+public class FadingCanvas : MonoBehaviour
 {
     public CanvasGroup optionsMenuCanvasGroup;
-    public float delayBeforeFadeIn = 0.0f;
+    public float delayBeforeFadeIn = 2.0f;
     public float fadeInDuration = 1f;
     public float delayBeforeFadeOut = 3f;
     public float fadeOutDuration = 1f;
 
-    // Start is called before the first frame update
+    // Start is called before first frame update
     void Start()
     {
-        // Start with the canvas fully transparent and not interactable
+        // Canvas starts transparent and is not interactable
         optionsMenuCanvasGroup.alpha = 0f;
         optionsMenuCanvasGroup.interactable = false;
         optionsMenuCanvasGroup.blocksRaycasts = false;
@@ -22,10 +22,10 @@ public class FadeCanvas : MonoBehaviour
 
     IEnumerator FadeInAndOutRoutine()
     {
-        // Wait for the specified delay before fading in
+        // Waiting for the delay before fading in
         yield return new WaitForSeconds(delayBeforeFadeIn);
 
-        // Fade in
+        // Fades in
         for (float t = 0.0f; t < fadeInDuration; t += Time.deltaTime)
         {
             optionsMenuCanvasGroup.alpha = Mathf.Lerp(0f, 1f, t / fadeInDuration);
@@ -35,10 +35,10 @@ public class FadeCanvas : MonoBehaviour
         optionsMenuCanvasGroup.interactable = true;
         optionsMenuCanvasGroup.blocksRaycasts = true;
 
-        // Wait for another delay before fading out
+        // Waiting for another delay before fading out
         yield return new WaitForSeconds(delayBeforeFadeOut);
 
-        // Fade out
+        // Fades out
         for (float t = 0.0f; t < fadeOutDuration; t += Time.deltaTime)
         {
             optionsMenuCanvasGroup.alpha = Mathf.Lerp(1f, 0f, t / fadeOutDuration);
